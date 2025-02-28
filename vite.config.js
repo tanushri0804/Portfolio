3eaf69e94769e5e4e4d7d58d5ea466adf5ea27c5
tanushri0804 <tanushri98371@gmail.com>
2025-02-28 14:20:48 +0530

    portfo

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/',  // Add a trailing slash
+  base: "/Portfolio/",  // Add a trailing slash
 });
