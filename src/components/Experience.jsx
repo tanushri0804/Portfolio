04f85f7609c433b3134e2e51fd398f1d975a8f3f
tanushri0804 <tanushri98371@gmail.com>
2025-05-12 20:17:28 +0530

    update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { FaTimes, FaExternalLinkAlt } from "react-icons/fa";
 import "./Experience.css";
 
